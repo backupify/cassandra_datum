df6a3a143a3fed1793ebb0b05ec0d2144a89ba5e
Alexander Litvinovsky <aliaksandr_litvinouski@epam.com>
2013-02-14 17:16:11 +0300

    fix typo in documentation

--- lib/cassandra_datum/acts_as_archive.rb
+++ lib/cassandra_datum/acts_as_archive.rb
@@ -13,7 +13,7 @@ module CassandraDatum
     # After you delete a record, it will be automatically replicated to cassandra in the following format
     #
     #   column_family will be 'deleted_records'
-    #   row_id will be the same as table name, for example above it will be 'users'
+    #   row_id will be the same as table name, for example above it will be 'services'
     #   column_name is the removal timestamp in unix format, it looks like '1360754413'
     #   column_attributes will contain the active record attributes
     #
