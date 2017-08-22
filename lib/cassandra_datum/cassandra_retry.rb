5e2c69fbf397b2e76613157690669d9e9818625e
Patrick McDermott <pmcdermott@datto.com>
2017-08-22 13:31:06 -0400

    remove twitter_cassandra

--- lib/cassandra_datum/cassandra_retry.rb
+++ lib/cassandra_datum/cassandra_retry.rb
@@ -1,6 +1,49 @@
 require 'exception_helper/retry'
 
 module CassandraDatum
+  module Exceptions
+    unless Object.const_defined?('::Thrift::Exception')
+      class ::Thrift::Exception < StandardError
+        def initialize(message)
+          super
+          @message = message
+        end
+
+        attr_reader :message
+      end
+    end
+
+    # This code is left here for documentation sake. We used to get TransportException
+    # from Thrift, but there is no equivalent with our CQL driver.
+    ##################################
+    # unless Object.const_defined?('Thrift::TransportException')
+    #   class Thrift::TransportException < Thrift::Exception
+    #     UNKNOWN = 0
+    #     NOT_OPEN = 1
+    #     ALREADY_OPEN = 2
+    #     TIMED_OUT = 3
+    #     END_OF_FILE = 4
+
+    #     attr_reader :type
+
+    #     def initialize(type = UNKNOWN, message = nil)
+    #       super(message)
+    #       @type = type
+    #     end
+    #   end
+    # end
+
+    unless Object.const_defined?('::ThriftClient::NoServersAvailable')
+      class ::ThriftClient::NoServersAvailable < Thrift::Exception
+      end
+    end
+
+    unless Object.const_defined?('::CassandraThrift::TimedOutException')
+      class ::CassandraThrift::TimedOutException < Thrift::Exception
+      end
+    end
+  end
+
   module CassandraRetry
     CASSANDRA_EXCEPTIONS = [
       ::Thrift::Exception,
